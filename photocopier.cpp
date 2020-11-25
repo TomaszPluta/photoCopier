cd0bce5f1dd417f012f72da9a1d8f23fc180a9ec
TomaszPluta <tomasz.pluta.g2@gmail.com>
2020-11-25 22:40:49 +0100

    finished

--- photocopier.cpp
+++ photocopier.cpp
@@ -33,13 +33,13 @@
 #include <algorithm>
 #include <iostream>
 #include <iomanip>
-
+#include <QScrollBar>
 
 
 namespace fs = std::filesystem;
 using namespace std::string_literals;
 
-void CopyFromDirs(const fs::path &dirInpput, const fs::path &dirOutput);
+void CopyFromDirs( QTextEdit *textEditOutput, const fs::path &dirInpput, const fs::path &dirOutput);
 
 PhotoCopier::PhotoCopier(QWidget *parent)
     : QMainWindow(parent)
@@ -76,23 +76,25 @@ void PhotoCopier::browseDestination(void)
 
 
 void PhotoCopier::startCopying(){
-    CopyFromDirs(ui->lineEditSource->text().toStdString(),  ui->lineEditDestination->text().toStdString());
+    CopyFromDirs(ui->textEditOutput, ui->lineEditSource->text().toStdString(),  ui->lineEditDestination->text().toStdString());
 }
 
 
 
 using recursive_directory_iterator = fs::recursive_directory_iterator;
 
-void CopyFromDirs(const fs::path &dirInpput, const fs::path &dirOutput){
+void CopyFromDirs( QTextEdit *textEditOutput, const fs::path &dirInpput, const fs::path &dirOutput){
       //fs::create_directories(argv[2]);
     int i = 0;
     fs::path dirOutputAbsoulte = fs::absolute(dirOutput);
     std::cout<<dirOutputAbsoulte;
+    textEditOutput->append(QString::fromStdString(std::string(dirOutputAbsoulte)));
     fs::create_directory(dirOutputAbsoulte);
     for (const auto& dirEntry : recursive_directory_iterator(dirInpput)){
         if (fs::is_regular_file(dirEntry)){
             fs::copy(dirEntry, dirOutput / dirEntry.path().filename() );
                 std::cout << "copied: "<<dirEntry << std::endl;
+                   textEditOutput->append(QString::fromStdString(std::string( "copied: " + std::string(dirEntry.path().string()) + " ")));
                 if (size_t pos = dirEntry.path().filename().string().find("BURST"); pos != std::string::npos){
                      std::string newFileName{dirEntry.path().filename().string()};
                      newFileName = newFileName.substr(pos + std::string("BURST").length());
@@ -100,13 +102,18 @@ void CopyFromDirs(const fs::path &dirInpput, const fs::path &dirOutput){
                      {
                         fs::rename(dirOutput / dirEntry.path().filename(), dirOutput / newFileName);
                         std::cout << " after renaming to:  "<<dirOutput / newFileName  << std::endl  << std::endl;
+                        std::string renamedPath (dirOutput / newFileName );
+                         textEditOutput->append(QString::fromStdString(std::string( " after renaming to:  " + renamedPath + "\n" )));
                      }
                      catch (fs::filesystem_error const& error)
                      {
                         std::cout << error.code() << "\n" << error.what() << "\n";
+                        textEditOutput->append(QString::fromStdString(std::string( "error  " + std::string(error.what() ) + "\n" )));
                      }
                 }else{
                     std::cout << " to:  "<<dirOutput / dirEntry.path().filename()  << std::endl  << std::endl;
+                    std::string renamedPath (dirOutput / dirEntry.path().filename() );
+                    textEditOutput->append(QString::fromStdString(std::string( " to:  " + renamedPath + "\n" )));
                 }
 
 
@@ -114,5 +121,8 @@ void CopyFromDirs(const fs::path &dirInpput, const fs::path &dirOutput){
         }
     }
     std::cout<<"copied: "<<i<< " files";
+     textEditOutput->append(QString::fromStdString("copied: " + std::to_string(i))+ "");
+      textEditOutput->append(QString("SUCCESS!\n"));
+      textEditOutput->verticalScrollBar()->setValue(textEditOutput->verticalScrollBar()->maximum());
 }
 
